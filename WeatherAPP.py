abec6300760c56bea6f9dda0d4bf1518def537d6
Ryan Rupakheti <rupakhetiryan@gmail.com>
2026-08-01 16:04:55 -0600

    Improved UI

--- WeatherAPP.py
+++ WeatherAPP.py
@@ -12,12 +12,15 @@ import calendar as cd
 BASE_URL1 = "https://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "d04a6beeb2871b7cddacc06477b407b3"
 
+
+
 # GUI Class for GUI functionality
 class AppGUI():
 
     #Check if searched is already clicked
     Already_Clicked = 0
 
+    #Intial Frame Setup
     def __init__(self):
 
         #Intializing GUI and adding demensions
@@ -29,6 +32,26 @@ class AppGUI():
 
         #Labels,Textboxes,and Buttons Below
         
+        #Setup for 3 x 3 Button Grid
+        self.ButtonGrid = tk.Frame(self.root)
+        self.ButtonGrid.pack(pady=10,fill='x')
+        self.ButtonGrid.columnconfigure(0,weight=1)
+        self.ButtonGrid.columnconfigure(1,weight=1)
+        self.ButtonGrid.columnconfigure(2,weight=1)
+        
+        # Create the buttons
+        self.SaveB = tk.Button(self.ButtonGrid, text="Save")
+        self.HomeB = tk.Button(self.ButtonGrid, text="Home")
+        self.ConvertB = tk.Button(self.ButtonGrid, text="Convert",)
+        
+        # Position them in one row
+        self.HomeB.grid(row=0, column=0, sticky="ew", padx=5)
+        self.SaveB.grid(row=0, column=1, sticky="ew", padx=5)
+        self.ConvertB.grid(row=0, column=2, sticky="ew", padx=5)
+
+        #Do not display intially
+        self.ButtonGrid.forget()
+
         #Intro Label Use as Initial Prompt
         self.Intro_Label = tk.Label(self.root,text="Enter a city to get Weather",font=("Arial",18))
         self.Intro_Label.pack(pady=10)
@@ -37,36 +60,33 @@ class AppGUI():
         self.textbox = tk.Text(self.root,height=1,font=("Arial",16))
         self.textbox.pack(padx=5,pady=5)
 
-        self.Search_B = tk.Button(self.root,text="Search",font=("Arial",18),command=self.City_Info)
+        #Serach button that displays information
+        self.Search_B = tk.Button(self.root,text="Search",font=("Arial",18),command=self.Display_Info)
         self.Search_B.pack()
 
         #This runs the GUI
         self.root.mainloop()
 
     #Get City Info
-    def City_Info(self):
+    def Display_Info(self):
 
         #Remove Intro_Label for now
         self.Intro_Label.forget()
+
+        #Put Button Frame on Top
+        self.ButtonGrid.pack(before=self.textbox)
 
         #Get City
         City_Input = self.textbox.get("1.0",tk.END).strip()
         # Then delete whats stored in textbox
         self.textbox.delete("1.0", tk.END)
 
-        #This is for the request
-        url1 = BASE_URL1 + "appid=" + API_KEY + "&q=" + City_Input
-
-        #Getting the current Data for said City
-        DailyForeCast = requests.get(url1).json()
-        #Get Current Country for City
-        City_Country = DailyForeCast['sys']['country']
-
         #Get all Data For Labels
-        Daily_T,Daily_FeelT,Daily_Max,Daily_Min,Daily_H,Daily_Des,Daily_W = Get_Weather_Data(DailyForeCast)
+        Daily_T,Daily_FeelT,Daily_Max,Daily_Min,Daily_H,Daily_Des,Daily_W,City_Country = Get_Weather_Data(City_Input)
 
         #If Already Clicked was 0
         if self.Already_Clicked == 0:
+
             #Create a CityLabel with text
             self.CityLabel = tk.Label(self.root,text=f"{City_Input},{City_Country}",font=("Arial",20))
             #Pack the label on screen
@@ -84,10 +104,10 @@ class AppGUI():
             self.FeelL = tk.Label(self.root,text=f"Feels Like: {Daily_FeelT}")
             self.FeelL.pack()
             
-            self.HumL = tk.Label(self.root,text=f"{Daily_H} %")
+            self.HumL = tk.Label(self.root,text=f"Humidity: {Daily_H} %")
             self.HumL.pack()
             
-            self.WinL = tk.Label(self.root,text=f"{Daily_W}")
+            self.WinL = tk.Label(self.root,text=f"Wind Speed: {Daily_W}")
             self.WinL.pack()
         else: 
             #Else config it to new City Value
@@ -99,19 +119,31 @@ class AppGUI():
             self.HumL.config(text=f"{Daily_H} %")
             self.WinL.config(text=f"{Daily_W}")
 
-
         #Increment the Already Clicked Class Var
         self.Already_Clicked += 1
 
-def Get_Weather_Data(Current_City):
+
+
+#Function to get all needed Weather Data for Display
+def Get_Weather_Data(City):
+
+  
+    url1 = BASE_URL1 + "appid=" + API_KEY + "&q=" + City + "&units=" + "metric"
+  
+
+    #Get the current info from city url
+    Current_City = requests.get(url1).json()
+
+    #Get country it is in
+    Country = Current_City['sys']['country']
 
     #Here we get Basic Daily ForCast Information 
-    City_Temp = Current_City['main']['temp']
-    City_Feels_Temp = Current_City['main']['feels_like']
-    City_Max = Current_City['main']['temp_max']
-    City_Min = Current_City['main']['temp_min']
+    City_Temp = round(Current_City['main']['temp'])
+    City_Feels_Temp = round(Current_City['main']['feels_like'])
+    City_Max = round(Current_City['main']['temp_max'])
+    City_Min = round(Current_City['main']['temp_min'])
     City_Humidity = Current_City['main']['humidity']
-    City_Description = Current_City['weather'][0]['description']
+    City_Description = Current_City['weather'][0]['description'].upper()
     City_Wind = Current_City['wind']['speed']
 
     # Get the UNIX vals for Sunris and Sunset
@@ -126,8 +158,8 @@ def Get_Weather_Data(Current_City):
     City_Sunrise = City_Sunrise.strftime("%I:%M%p")
     City_Sunset = City_Sunset.strftime("%I:%M%p")
 
-    return City_Temp,City_Feels_Temp,City_Max,City_Min,City_Humidity,City_Description,City_Wind
-
+    #Return Needed Information
+    return City_Temp,City_Feels_Temp,City_Max,City_Min,City_Humidity,City_Description,City_Wind,Country
 
 #Main 
 def main():
@@ -137,7 +169,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-# {'coord': {'lon': 51.4215, 'lat': 35.6944}, 'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky', 'icon': '01n'}], 'base': 'stations', 'main': {'temp': 306.88, 'feels_like': 304.43, 'temp_min': 306.88, 'temp_max': 306.88, 'pressure': 1010, 'humidity': 13, 'sea_level': 1010, 'grnd_level': 863}, 'visibility': 10000, 'wind': {'speed': 1.79, 'deg': 180}, 'clouds': {'all': 0}, 'dt': 1785607825, 'sys': {'type': 2, 'id': 47737, 'country': 'IR', 'sunrise': 1785548516, 'sunset': 1785598755}, 'timezone': 12600, 'id': 112931, 'name': 'Tehran', 'cod': 200}
